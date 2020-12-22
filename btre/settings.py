954209d0545dfeb3763efebc91907b588bc912ba
Surajit Das <surajit.cosh@gmail.com>
2020-12-22 23:47:00 +0530

    Final Commit

--- btre/settings.py
+++ btre/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -145,3 +146,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#Email Config
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '' #Sender Email REQUIRED
+EMAIL_HOST_PASSWORD='' #Sender Password REQUIRED
+EMAIL_USE_TLS=True
